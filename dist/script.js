13c0b198e4cc94c0aa8e6344b80a5060a6ebb299
Koushyar <koushyarheidaribateni@gmail.com>
2024-03-14 03:14:16 +0330

    functionality for action buttons added

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,18 @@
-const tasksArr = [];
-const addTask = document.getElementById("add-task");
+let tasksArr = [];
+let editFlag = 0;
+let idOftaskBeingEdited;
+const addTaskBtn = document.getElementById("add-task");
 const overLay = document.querySelector(".overlay");
-addTask.addEventListener("click", showModal);
+addTaskBtn.addEventListener("click", addTask);
+
+function addTask() {
+  const modalBtnDiv = overLay.querySelector(".modal-btn");
+  modalBtnDiv.classList.remove("hidden");
+  const modalBtn = modalBtnDiv.querySelector("button");
+  modalBtn.textContent = "Add Task";
+  showModal();
+}
+
 function showModal() {
   overLay.classList.remove("hidden");
 }
@@ -19,18 +30,29 @@ let counter = 0;
 function handleModalForm(e) {
   e.preventDefault();
   const { taskName, priority, status, date } = e.target;
-  console.log(taskName.value, priority.value, status.value, date.value);
-  let task = {
-    name: taskName.value,
-    priority: priority.value,
-    status: status.value,
-    date: date.value,
-    id: counter,
-  };
-  counter++;
-  tasksArr.push(task);
-  console.log(tasksArr);
+  if (editFlag === 0) {
+    let task = {
+      name: taskName.value,
+      priority: priority.value,
+      status: status.value,
+      date: date.value,
+      id: counter,
+    };
+    counter++;
+    tasksArr.push(task);
+  } else {
+    tasksArr.forEach((task) => {
+      if (task.id === idOftaskBeingEdited) {
+        task.name = taskName.value;
+        task.priority = priority.value;
+        task.status = status.value;
+        task.date = date.value;
+      }
+      editFlag = 0;
+    });
+  }
   renderTasks(tasksArr);
+  e.target.reset();
 }
 
 function renderTasks(arr) {
@@ -39,7 +61,7 @@ function renderTasks(arr) {
   tasksTableBody.innerHTML = "";
   arr.forEach((task) => {
     const taskRow = document.createElement("tr");
-
+    taskRow.id = task.id;
     const taskNameCell = document.createElement("td");
     taskNameCell.classList.add("body-cell");
     const taskNameP = document.createElement("p");
@@ -72,18 +94,25 @@ function renderTasks(arr) {
     actionsCell.classList.add("body-cell");
     const actionsWrapper = document.createElement("div");
     actionsWrapper.classList.add("flex", "gap-1", "justify-center");
+
     const deleteBtn = document.createElement("img");
     deleteBtn.classList.add("delete-btn");
     deleteBtn.src = "../assets/icons/delete.svg";
     deleteBtn.alt = "delete";
+    deleteBtn.addEventListener("click", handleDelete);
+
     const editBtn = document.createElement("img");
     editBtn.classList.add("edit-btn");
     editBtn.src = "../assets/icons/edit.svg";
     editBtn.alt = "edit";
+    editBtn.addEventListener("click", handleEdit);
+
     const showBtn = document.createElement("img");
     showBtn.classList.add("show-btn");
     showBtn.src = "../assets/icons/show.svg";
     showBtn.alt = "show";
+    showBtn.addEventListener("click", handleShow);
+
     actionsWrapper.append(deleteBtn, editBtn, showBtn);
     actionsCell.append(actionsWrapper);
 
@@ -96,4 +125,42 @@ function renderTasks(arr) {
     );
     tasksTableBody.append(taskRow);
   });
+}
+
+function handleDelete(e) {
+  const taskId = e.target.parentElement.parentElement.parentElement.id;
+  tasksArr = tasksArr.filter((task) => String(task.id) !== taskId);
+  e.target.parentElement.parentElement.parentElement.remove();
+}
+
+function handleShow(e) {
+  auxiliaty(e);
+  const modalBtnDiv = overLay.querySelector(".modal-btn");
+  modalBtnDiv.classList.add("hidden");
+  showModal();
+}
+
+function handleEdit(e) {
+  const modalBtnDiv = overLay.querySelector(".modal-btn");
+  modalBtnDiv.classList.remove("hidden");
+  const modalBtn = modalBtnDiv.querySelector("button");
+  modalBtn.textContent = "Edit Task";
+  idOftaskBeingEdited = auxiliaty(e);
+  showModal();
+  editFlag = 1;
+}
+
+function auxiliaty(e) {
+  const taskId = e.target.parentElement.parentElement.parentElement.id;
+  const taskObj = tasksArr.find((task) => String(task.id) === taskId);
+  const taskName = overLay.querySelector("input[name='taskName']");
+  taskName.value = taskObj.name;
+  const taskPriority = overLay.querySelector("select[name='priority']");
+  taskPriority.value = taskObj.priority;
+  const taskStatus = overLay.querySelector("select[name='status']");
+  taskStatus.value = taskObj.status;
+  const taskDate = overLay.querySelector("input[name='date']");
+  taskDate.value = taskObj.date;
+
+  return Number(taskId);
 }
